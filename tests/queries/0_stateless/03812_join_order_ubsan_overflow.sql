11c678720fad6f36d8d1add21c8bddad65592118
Alexey Milovidov <milovidov@clickhouse.com>
2026-02-02 00:05:38 +0100

    Fix flaky test 03812_join_order_ubsan_overflow.sql

--- tests/queries/0_stateless/03812_join_order_ubsan_overflow.sql
+++ tests/queries/0_stateless/03812_join_order_ubsan_overflow.sql
@@ -7,6 +7,7 @@ DROP TABLE IF EXISTS data_03812;
 CREATE TABLE data_03812 (key UInt64, value UInt64) ENGINE = MergeTree ORDER BY key;
 INSERT INTO data_03812 VALUES (1, 1), (2, 2);
 
+SET max_rows_to_read = 0;
 
 EXPLAIN PLAN
 SELECT 1
