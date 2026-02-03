9fb438f55eab8f0e4b097a0ad81ed8f4edeaad95
Alexei Fedotov <alexei.fedotov@clickhouse.com>
2026-02-03 12:18:49 +0100

    Change a test to test

--- ci/praktika/runner.py
+++ ci/praktika/runner.py
@@ -426,7 +426,7 @@ class Runner:
             uid = os.getuid()
             gid = os.getgid()
             chown_cmd = f"docker run --rm --user root --volume ./:{current_dir} --workdir={current_dir} {docker} chown -R {uid}:{gid} {Settings.TEMP_DIR}"
-            print(f"--- Run ownership fix command [{chown_cmd}]")
+            print(f"--- Run ownership fix command")
             Shell.check(chown_cmd, verbose=True)
 
         return exit_code
--- tests/queries/0_stateless/00001_select_1.sql
+++ tests/queries/0_stateless/00001_select_1.sql
@@ -1,1 +1,2 @@
+--REMOVEME
 SELECT 1
