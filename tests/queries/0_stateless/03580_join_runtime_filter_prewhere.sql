5389bad2c343264ade1523393f293aa37ae33f5a
Alexander Gololobov <davenger@clickhouse.com>
2026-02-03 09:17:29 +0100

    Test for adding runtime filter along with other PREWHERE condition

--- tests/queries/0_stateless/03580_join_runtime_filter_prewhere.sql
+++ tests/queries/0_stateless/03580_join_runtime_filter_prewhere.sql
@@ -41,3 +41,15 @@ FROM
     ))
 )
 WHERE (explain LIKE '%ReadFromMergeTree%') OR (explain LIKE '%Prewhere filter column:%') OR (explain LIKE '%Build%');
+
+
+SELECT '-- Addintional filter in PREWHERE';
+SELECT REGEXP_REPLACE(trimLeft(explain), '_runtime_filter_\\d+', '_runtime_filter_UNIQ_ID')
+FROM
+(
+    EXPLAIN actions=1
+    SELECT count() FROM nation, customer
+    WHERE (c_nationkey = n_nationkey) AND (n_name = 'FRANCE') AND (c_custkey%4 = 0)
+    SETTINGS enable_join_runtime_filters = 1, optimize_move_to_prewhere = 1, query_plan_join_swap_table = 1
+)
+WHERE (explain LIKE '%ReadFromMergeTree%') OR (explain LIKE '%Prewhere filter column:%') OR (explain LIKE '%Build%');
