4191a6262c1ad7e02de5f24d79e32466dac5e97d
Raúl Marín <raul.marin@clickhouse.com>
2026-02-06 17:12:00 +0100

    Hide expensive magic instantiation

--- src/Interpreters/Cache/FileSegmentKeyType.h
+++ src/Interpreters/Cache/FileSegmentKeyType.h
@@ -1,25 +1,18 @@
 #pragma once
 
-#include <base/EnumReflection.h>
+#include <base/types.h>
 
 namespace DB
 {
-    enum class FileSegmentKeyType : uint8_t
-    {
-        General = 0,
-        System, // Segment for (metadata, index, marks, etc.)
-        Data, // Segment for table data
-    };
 
-    inline String getKeyTypePrefix(FileSegmentKeyType type)
-    {
-        if (type == FileSegmentKeyType::General)
-            return "";
-        return String(magic_enum::enum_name(type));
-    }
+enum class FileSegmentKeyType : uint8_t
+{
+    General = 0,
+    System, // Segment for (metadata, index, marks, etc.)
+    Data, // Segment for table data
+};
 
-    inline String toString(FileSegmentKeyType type)
-    {
-        return String(magic_enum::enum_name(type));
-    }
+String getKeyTypePrefix(FileSegmentKeyType type);
+String toString(FileSegmentKeyType type);
+
 }
