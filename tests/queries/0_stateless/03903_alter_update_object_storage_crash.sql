034a1f8cd314d2e6119595b5bce29cdaba0faa81
Alexey Milovidov <milovidov@clickhouse.com>
2026-02-06 07:44:20 +0100

    Update 03903_alter_update_object_storage_crash.sql

--- tests/queries/0_stateless/03903_alter_update_object_storage_crash.sql
+++ tests/queries/0_stateless/03903_alter_update_object_storage_crash.sql
@@ -7,7 +7,7 @@
 DROP TABLE IF EXISTS t_object_storage_update;
 
 CREATE TABLE t_object_storage_update (c0 Int32)
-ENGINE = S3(s3_conn, filename = 'test_03903_alter_update.parquet', format = Parquet);
+ENGINE = S3(s3_conn, filename = currentDatabase() || '_test_03903_alter_update.parquet', format = Parquet);
 
 INSERT INTO t_object_storage_update VALUES (0);
 
