7e1b927dd3007e9689b08a3b197d9e9842da8640
Nikita Fomichev <nikita.fomichev@clickhouse.com>
2026-02-06 21:34:31 +0100

    Stress tests: add hardenings: --replace-log-memory-with-mergetree and async_insert=1

--- ci/jobs/scripts/stress/stress.py
+++ ci/jobs/scripts/stress/stress.py
@@ -218,6 +218,9 @@ def get_options(i: int, upgrade_check: bool, encrypted_storage: bool) -> str:
     if random.random() < 0.2:
         client_options.append("async_insert=1")
 
+    if random.random() < 0.05:
+        client_options.append("enable_join_runtime_filters=1")
+
     # dpsize' - implements DPsize algorithm currently only for Inner joins. So it may not work in some tests.
     # That is why we use it with fallback to 'greedy'.
     join_order_algorithm_combinations = ["greedy", "dpsize,greedy", "greedy,dpsize"]
