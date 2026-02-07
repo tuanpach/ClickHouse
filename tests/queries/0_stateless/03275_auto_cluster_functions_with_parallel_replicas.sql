38e52a3004599c4423fa97cd9faa9dac66396d7b
phulv94 <vinhphuluong94@gmail.com>
2026-02-07 19:35:40 +0700

    fix type

--- tests/queries/0_stateless/03275_auto_cluster_functions_with_parallel_replicas.sql
+++ tests/queries/0_stateless/03275_auto_cluster_functions_with_parallel_replicas.sql
@@ -4,7 +4,7 @@
 SET enable_analyzer=1;
 SET enable_parallel_replicas=1;
 SET max_parallel_replicas=4;
-SET cluster_for_parallel_replicas='my_cluster';
+SET cluster_for_parallel_replicas='test_cluster_two_shards';
 SET query_plan_join_swap_table=0;
 SET enable_join_runtime_filters=0;
 
