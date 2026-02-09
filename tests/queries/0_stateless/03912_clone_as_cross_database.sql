f3fe8bd70a11f4f443858891d9ad3a2c93625d92
Hasyimi Bahrudin <hasyimi@raccoons.dev>
2026-02-09 14:28:03 +0800

    Fix test

--- tests/queries/0_stateless/03912_clone_as_cross_database.sql
+++ tests/queries/0_stateless/03912_clone_as_cross_database.sql
@@ -1,15 +1,15 @@
 -- Tags: no-replicated-database
 -- Tag no-replicated-database: Unsupported type of CREATE TABLE ... CLONE AS ... query
 
-DROP DATABASE IF EXISTS {CLICKHOUSE_DATABASE_1:Identifier};
-CREATE DATABASE {CLICKHOUSE_DATABASE_1:Identifier};
+DROP DATABASE IF EXISTS clone_as_source_03912;
+CREATE DATABASE clone_as_source_03912;
 
-CREATE TABLE {CLICKHOUSE_DATABASE_1:Identifier}.source (x Int8, y String) ENGINE = MergeTree PRIMARY KEY x;
-INSERT INTO {CLICKHOUSE_DATABASE_1:Identifier}.source VALUES (1, 'a'), (2, 'b'), (3, 'c');
+CREATE TABLE clone_as_source_03912.source (x Int8, y String) ENGINE = MergeTree PRIMARY KEY x;
+INSERT INTO clone_as_source_03912.source VALUES (1, 'a'), (2, 'b'), (3, 'c');
 
 -- CLONE AS with a fully-qualified source table from another database
-CREATE TABLE clone_target CLONE AS {CLICKHOUSE_DATABASE_1:Identifier}.source;
+CREATE TABLE clone_target CLONE AS clone_as_source_03912.source;
 SELECT * FROM clone_target ORDER BY x;
 
 DROP TABLE clone_target;
-DROP DATABASE {CLICKHOUSE_DATABASE_1:Identifier};
+DROP DATABASE clone_as_source_03912;
