a872ae60843c36bcd40f8ea2aae117fe36964a06
serxa <sergei@clickhouse.com>
2026-02-09 15:32:51 +0000

    Fix the test for livelock

--- tests/queries/0_stateless/03903_cancellation_checker_large_timeout.sql
+++ tests/queries/0_stateless/03903_cancellation_checker_large_timeout.sql
@@ -3,5 +3,7 @@
 -- The timeout is internally capped to 1 year to prevent overflow in std::condition_variable::wait_for.
 -- This query should complete quickly without hanging, regardless of the huge timeout value.
 
-SET max_execution_time = 9223372036854775;  -- Close to INT64_MAX / 1000, would overflow when converted to nanoseconds
+SET max_execution_time = 9223372041;  -- Close to INT64_MAX / 1000000000, would overflow when converted to nanoseconds
+SELECT 1;
+SELECT 1;
 SELECT 1;
