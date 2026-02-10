98cc1dc9aa4268ebcbf607d97b860cbc49c79b5a
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2026-02-10 14:47:54 +0000

    Another fix

--- src/Client/BuzzHouse/Generator/SQLTable.cpp
+++ src/Client/BuzzHouse/Generator/SQLTable.cpp
@@ -22,7 +22,7 @@ static void collectNullable(SQLType * tp, const uint32_t flags, ColumnPathChain 
         tp = lc->subtype;
     }
     if ((flags & collect_generated) != 0
-        && (tp && tp->getTypeClass() == SQLTypeClass::NULLABLE
+        && ((tp && tp->getTypeClass() == SQLTypeClass::NULLABLE)
             || (tp && tp->getTypeClass() == SQLTypeClass::TUPLE && dynamic_cast<TupleType *>(tp)->nullable)))
     {
         next.path.emplace_back(ColumnPathChainEntry("null", &(*null_tp)));
