48234ff81a6d2a6030b5432f83410124370983fd
Alexei Fedotov <alexei.fedotov@clickhouse.com>
2026-02-10 18:26:40 +0100

    Use correct mount in the job, r

--- ci/jobs/ast_fuzzer_job.py
+++ ci/jobs/ast_fuzzer_job.py
@@ -83,7 +83,7 @@ def run_fuzz_job(check_name: str):
     logging.info("Buzzhouse: Fixing file ownership after running docker as root")
     uid = os.getuid()
     gid = os.getgid()
-    chown_cmd = f"docker run --rm --user root --volume {cwd}:/repo --workdir=/repo {docker_image} chown -R {uid}:{gid} {temp_dir}"
+    chown_cmd = f"docker run --rm --user root --volume {cwd}:/repo --workdir=/repo {docker_image} chown -R {uid}:{gid} /repo"
     Shell.check(chown_cmd, verbose=True)
 
     fuzzer_log = workspace_path / "fuzzer.log"
