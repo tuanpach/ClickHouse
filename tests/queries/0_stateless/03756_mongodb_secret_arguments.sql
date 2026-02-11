a7533e5c510560103e57a9a602dce5d31e1179c8
Pervakov Grigorii <pervakov.grigory@gmail.com>
2026-02-11 20:10:20 +0000

    Fix test

--- tests/queries/0_stateless/03756_mongodb_secret_arguments.sql
+++ tests/queries/0_stateless/03756_mongodb_secret_arguments.sql
@@ -1,3 +1,3 @@
 -- Tags: no-fasttest
 
-SELECT * FROM mongodb(some_named_collection, now()); -- { serverError BAD_ARGUMENTS }
+SELECT * FROM mongodb(some_named_collection, now()); -- { serverError NAMED_COLLECTION_DOESNT_EXIST }
